e5dd162c27037b54773ecadea6cd7db64a636b52
Alija1 <58340674+Alija1@users.noreply.github.com>
2020-06-01 20:48:24 +0200

    Add files via upload

---
+++ zapreminaValjka.js
@@ -0,0 +1,5 @@
+var numberR = prompt(`Please enter r:`);
+var numberH = prompt(`Please enter H:`);
+
+var v = parseInt(numberR) * parseInt(numberR) * Math.PI * parseInt(numberH);
+alert (`V = ` + v);

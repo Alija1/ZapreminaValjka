7926e50c42b58814a46e2b6f2891c976d0dcb8f1
Alija1 <58340674+Alija1@users.noreply.github.com>
2020-06-01 20:31:36 +0200

    Delete zapreminaValjka.js

--- zapreminaValjka.js
+++
@@ -1,7 +0,0 @@
-var numberR = prompt(`Please enter r:`);
-var numberH = prompt(`Please enter H:`);
-let numberRe2 = parseInt(numberR) * e2;
-const NUMBER_PI = `3,14`;
-
-var v = numberRe2 * parseInt(numberH) * NUMBER_PI;
-alert("V = " + v);
